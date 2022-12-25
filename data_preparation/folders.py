509afa40e59e45984d61b1c044e22c18ce1d7150
AlexandraKol <95126104+AlexandraKol@users.noreply.github.com>
2022-12-25 21:34:53 +0500

    Delete folders.py

--- data_preparation/folders.py
+++
@@ -1,5 +0,0 @@
-import splitfolders
-
-input_folder = "D:/ЧелГУ/Практика/Фото1000"
-
-splitfolders.ratio(input_folder, 'D:/ЧелГУ/Практика/Фото1000', ratio = (0.7, 0.2, 0.1), seed=13, group_prefix=None)
